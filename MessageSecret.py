5d53232e8e66532fa96ed72bc402f1850696d1c2
Christophe Roux <cri@live.ca>
2019-02-28 16:19:05 -0500

    Encryption d'un message

--- MessageSecret.py
+++ MessageSecret.py
@@ -9,7 +9,8 @@ key = int(key)
 
 message = input('Veuillez entrer un message: ')
 
-position = alphabet.find(character)
-newPosition = (position + key) % 26
-newCharacter = alphabet[newPosition]
-print('Le nouveau caractère est: ' + newCharacter)
+for character in message:
+    position = alphabet.find(character)
+    newPosition = (position + key) % 26
+    newCharacter = alphabet[newPosition]
+    print('Le nouveau caractère est: ' + newCharacter)
